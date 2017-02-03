020b0f415648f0391cc7a11bd7f5cc089bac5fd5
ajsteadly <ameliajstead@gmail.com>
2017-02-03 16:19:18 -0400

    Further troubleshooting for rename.

--- src/main/java/com/vaadin/tutorial/addressbook/backend/Task.java
+++ src/main/java/com/vaadin/tutorial/addressbook/backend/Task.java
@@ -13,68 +13,19 @@ import java.util.Date;
  */
 // Backend DTO class. This is just a typical Java backend implementation
 // class and nothing Vaadin specific.
-public class Contact implements Serializable, Cloneable {
+public class Task implements Serializable, Cloneable {
 
     private Long id;
-
     private String firstName = "";
     private String lastName = "";
-    private String phone = "";
-    private String email = "";
-    private Date birthDate;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Date getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(Date birthDate) {
-        this.birthDate = birthDate;
-    }
+    private String taskName = "";
+    private Date startDate;
+    private Date expectedEndDate;
 
     @Override
-    public Contact clone() throws CloneNotSupportedException {
+    public Task clone() throws CloneNotSupportedException {
         try {
-            return (Contact) BeanUtils.cloneBean(this);
+            return (Task) BeanUtils.cloneBean(this);
         } catch (Exception ex) {
             throw new CloneNotSupportedException();
         }
@@ -82,9 +33,57 @@ public class Contact implements Serializable, Cloneable {
 
     @Override
     public String toString() {
-        return "Contact{" + "id=" + id + ", firstName=" + firstName
-                + ", lastName=" + lastName + ", phone=" + phone + ", email="
-                + email + ", birthDate=" + birthDate + '}';
+        return "Task{" + "id=" + id + ", firstName=" + firstName
+                + ", lastName=" + lastName + ", taskName=" + taskName + ", startDate="
+                + startDate + ", expectedEndDate=" + expectedEndDate + '}';
     }
+
+    public Long getId() {
+    	return id;
+    }
+    
+    public void setId(Long id) {
+    	this.id = id;
+    }
+    
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getTask() {
+		return taskName;
+	}
+
+	public void setTask(String task) {
+		this.taskName = task;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getExpectedEndDate() {
+		return expectedEndDate;
+	}
+
+	public void setExpectedEndDate(Date expectedEndDate) {
+		this.expectedEndDate = expectedEndDate;
+	}
 
 }
--- src/main/java/com/vaadin/tutorial/addressbook/backend/TaskService.java
+++ src/main/java/com/vaadin/tutorial/addressbook/backend/TaskService.java
@@ -13,7 +13,7 @@ import java.util.logging.Logger;
  */
 // Backend service class. This is just a typical Java backend implementation
 // class and nothing Vaadin specific.
-public class ContactService {
+public class TaskService {
 
     // Create dummy data by randomly combining first and last names
     static String[] fnames = { "Peter", "Alice", "John", "Mike", "Olivia",
@@ -24,40 +24,42 @@ public class ContactService {
             "Brown", "Davis", "Miller", "Wilson", "Moore", "Taylor",
             "Anderson", "Thomas", "Jackson", "White", "Harris", "Martin",
             "Thompson", "Young", "King", "Robinson" };
+    static String[] tasks = {"T.1.1", "T.1.2", "T.1.3", "T.1.4",
+    		"T.2.1", "T.2.2", "T.2.3", 
+    		"T.3.1", "T.3.2",
+    		"T.4.1", "T.4.2", "T.4.3", "T.4.4", "T.4.5",
+    		"T.5.1"};
 
-    private static ContactService instance;
+    private static TaskService instance;
 
-    public static ContactService createDemoService() {
+    public static TaskService createDemoService() {
         if (instance == null) {
 
-            final ContactService contactService = new ContactService();
+            final TaskService taskService = new TaskService();
 
             Random r = new Random(0);
             Calendar cal = Calendar.getInstance();
-            for (int i = 0; i < 100; i++) {
-                Contact contact = new Contact();
-                contact.setFirstName(fnames[r.nextInt(fnames.length)]);
-                contact.setLastName(lnames[r.nextInt(fnames.length)]);
-                contact.setEmail(contact.getFirstName().toLowerCase() + "@"
-                        + contact.getLastName().toLowerCase() + ".com");
-                contact.setPhone("+ 358 555 " + (100 + r.nextInt(900)));
-                cal.set(1930 + r.nextInt(70),
-                        r.nextInt(11), r.nextInt(28));
-                contact.setBirthDate(cal.getTime());
-                contactService.save(contact);
+            for (int i = 0; i < tasks.length; i++) {
+                Task task = new Task();
+                task.setFirstName(fnames[r.nextInt(fnames.length)]);
+                task.setLastName(lnames[r.nextInt(fnames.length)]);
+                task.setTask(tasks[i]);
+                task.setStartDate(cal.getTime());
+                task.setExpectedEndDate(new Date(System.currentTimeMillis() + System.currentTimeMillis()));
+                taskService.save(task);
             }
-            instance = contactService;
+            instance = taskService;
         }
 
         return instance;
     }
 
-    private HashMap<Long, Contact> contacts = new HashMap<>();
+    private HashMap<Long, Task> contacts = new HashMap<>();
     private long nextId = 0;
 
-    public synchronized List<Contact> findAll(String stringFilter) {
-        ArrayList arrayList = new ArrayList();
-        for (Contact contact : contacts.values()) {
+    public synchronized List<Task> findAll(String stringFilter) {
+        ArrayList<Task> arrayList = new ArrayList<Task>();
+        for (Task contact : contacts.values()) {
             try {
                 boolean passesFilter = (stringFilter == null || stringFilter.isEmpty())
                         || contact.toString().toLowerCase()
@@ -66,14 +68,14 @@ public class ContactService {
                     arrayList.add(contact.clone());
                 }
             } catch (CloneNotSupportedException ex) {
-                Logger.getLogger(ContactService.class.getName()).log(
+                Logger.getLogger(TaskService.class.getName()).log(
                         Level.SEVERE, null, ex);
             }
         }
-        Collections.sort(arrayList, new Comparator<Contact>() {
+        Collections.sort(arrayList, new Comparator<Task>() {
 
             @Override
-            public int compare(Contact o1, Contact o2) {
+            public int compare(Task o1, Task o2) {
                 return (int) (o2.getId() - o1.getId());
             }
         });
@@ -84,16 +86,16 @@ public class ContactService {
         return contacts.size();
     }
 
-    public synchronized void delete(Contact value) {
+    public synchronized void delete(Task value) {
         contacts.remove(value.getId());
     }
 
-    public synchronized void save(Contact entry) {
+    public synchronized void save(Task entry) {
         if (entry.getId() == null) {
             entry.setId(nextId++);
         }
         try {
-            entry = (Contact) BeanUtils.cloneBean(entry);
+            entry = (Task) BeanUtils.cloneBean(entry);
         } catch (Exception ex) {
             throw new RuntimeException(ex);
         }
